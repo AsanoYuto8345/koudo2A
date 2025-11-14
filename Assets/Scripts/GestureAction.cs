01eff83c714f61e5be00d94448ce5ca649339422
Taewoong Han <taewoong@taewoong.me>
2025-11-14 11:35:34 +0900

    Add acript for actions

---
+++ Assets/Scripts/GestureAction.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class GestureAction : MonoBehaviour
+{
+    [SerializeField]
+    string gestureName;
+
+    [SerializeField]
+    bool isGestureActive = false;
+
+    [SerializeField]
+    GameObject hand;
+
+    Vector3 handInitialPosition;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (isGestureActive)
+        {
+            switch (gestureName)
+            {
+                case "OpenHand":
+                    Debug.Log("Open Hand Gesture Activated");
+                    break;
+                case "Fist":
+                    Debug.Log("Fist Gesture Activated");
+                    break;
+                case "Shild":
+                    Debug.Log("Shield Gesture Activated");
+                    break;
+                case "Lightbulb":
+                    Debug.Log("Lightbulb Gesture Activated");
+                    break;
+            }
+        }
+    }
+
+    private bool MovedOverVector(Vector3 vector)
+    {
+        return hand.transform.position.z - handInitialPosition.z > vector.z &&
+               hand.transform.position.y - handInitialPosition.y > vector.y &&
+               hand.transform.position.x - handInitialPosition.x > vector.x;
+    }
+
+    public void ActivateGesture()
+    {
+        isGestureActive = true;
+        handInitialPosition = hand.transform.position;
+    }
+
+    public void InactivateGesture()
+    {
+        isGestureActive = false;
+    }
+}
